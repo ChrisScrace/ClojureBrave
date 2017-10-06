3f6882f9f0ac53e50345fb0a8fa6bedee79c8a56
Christopher Scrace <chris_scrace@slc.co.uk>
2017-10-06 15:53:52 +0100

    changes

--- clojure-noob/project.clj
+++ clojure-noob/project.clj
@@ -1,9 +1,10 @@
 (defproject clojure-noob "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
+  :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options"]
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot clojure-noob.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
