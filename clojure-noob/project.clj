456f97cf15b69b97918c9468cab106ba9301a798
Christopher Scrace <chris_scrace@slc.co.uk>
2017-10-09 10:07:04 +0100

    project setup

--- clojure-noob/project.clj
+++ clojure-noob/project.clj
@@ -5,6 +5,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
+
+  :mirrors {"central" {:name "SLC Artifactory (Maven Central)"
+                       :url  "http://artifactory.slc.co.uk:8080/artifactory/repo"}
+            "clojars" {:name "SLC Artifactory (Clojars)"
+                       :url  "http://artifactory.slc.co.uk:8080/artifactory/ext-release-local"}
+            "local"   {:name "LOCAL"
+                       :url  "file:///home/dev/.gradle"}}
+
   :main ^:skip-aot clojure-noob.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
